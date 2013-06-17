421f6b3e147d5929ac992c2a8f32d8672f2b0f39
Ronald Osure <sureronald@gmail.com>
2013-06-17 06:09:38 +0300

    iU-2013.17.06-R-0 Commencement, write base code

---
+++ flow_handler.py
@@ -0,0 +1,15 @@
+# Triggered by Bro system call to notify web interface
+
+import re
+
+__author__ = 'Ronald Osure (rosure@indiana.edu)'
+__copyright__ = 'Copyright 2012, InCNTRE. This file is licensed under Apache 2.0'
+
+class FlowHandler:
+    pass
+
+def main():
+    
+if __name__ == '__main__':
+    main()
+
