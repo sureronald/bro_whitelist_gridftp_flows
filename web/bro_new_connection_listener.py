a913c7a25ae94b3515c246c493ed918defc7ea6a
Ronald Osure <sureronald@gmail.com>
2013-06-18 18:40:10 +0300

    iU-2013.17.06-R-0 Start work on listener socket to process flows (put to db & notify flowscale rest interface

---
+++ web/bro_new_connection_listener.py
@@ -0,0 +1,50 @@
+# Listener for new connection params (JSON formatted) from Bro & process
+
+import logging
+import SocketServer
+import json
+
+__author__ = 'Ronald Osure (rosure@indiana.edu)'
+__copyright__ = 'Copyright 2012, InCNTRE. This file is licensed under Apache 2.0'
+
+HOST_PORT = ('localhost', 2007,)
+
+class BroConnectionTcpHandler(SocketServer.BaseRequestHandler):
+    """
+    TCP Handler Class
+    """
+    
+    def _json_decode(self,data):
+        try:
+            json_data = json.loads(data)
+        except ValueError,e:
+            #logging.error("Failed to decode:"+data)
+            return False
+        return json_data
+    
+    def handle(self):
+	   pass
+    
+    def finish(self):
+        pass
+
+class SocketController:
+    """
+        Socket Controller
+    """
+    
+    def __init__(self):
+        pass
+    
+    def start_socket(self):
+        #logging.debug("Vtiger OpenNMS Integration Server started\n-------------------------------------------------------")
+        server = SocketServer.ThreadingTCPServer(HOST_PORT, BroConnectionTcpHandler)
+        server.serve_forever()
+    
+    def shutdown_socket(self):
+        server.shutdown()
+
+if __name__ == '__main__':
+    socket_controller = SocketController()
+    socket_controller.start_socket()
+
