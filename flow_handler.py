2ed268fec4996dd5e5492c1c93b3fd70252ff81e
Ronald Osure <sureronald@gmail.com>
2013-06-24 06:29:53 +0300

    iU-2013.23.06-R-0 Implement flows cache data structure, commence work on algorithm to populate the data structure during runtime

--- flow_handler.py
+++ flow_handler.py
@@ -1,5 +1,6 @@
 # Triggered by Bro system call to notify web interface
 
+import time
 import sys
 import re
 import SocketServer
@@ -12,6 +13,22 @@ __copyright__ = 'Copyright 2012, InCNTRE. This file is licensed under Apache 2.0
 
 SERVER = None
 HOST_PORT = ('localhost', 1999,)
+
+#Flows data structure (note that the keys for access concatenates the orig_h and resp_h variables for quick access)
+#We keep track of communicating ports just to reduce the number of rules we send to the switch
+# FLOWS_CACHE = { 'orig_h-resp_h': {
+#                                    'orig_p_range: [min, high],
+#                                    'resp_p_range: [min, high],
+#                                    'time': time.time() #Used for expiring values from the flows cache based on FLOW_CACHE_MAX_TIME
+#                                   }
+#               }
+
+FLOWS_CACHE = {}
+
+#Time it takes before a flow is dropped from the FLOWS_CACHE, default is 15 minutes (We can change this, the best value
+#would be the average time it takes for gridftp traffic to complete transferring data)
+
+FLOW_CACHE_MAX_TIME = 900 
 
 #Logging Settings
 
@@ -58,9 +75,27 @@ class FlowTcpHandler(SocketServer.BaseRequestHandler):
         #Cast matching group tuples list to dict
         #Expected sample result after cast: {'orig_p': '35030/tcp', 'orig_h': '149.159.4.26', 'resp_p': '80/tcp', 'resp_h': '149.165.180.10'}
         
-        connection_object = dict(match_group)
+        conn_obj = dict(match_group)
         
-        logging.debug(connection_object)
+        #logging.debug(connection_object)
+        
+        #We now add a flow to the data structure if it does not exist or update port range if needed
+        #If communication is on port 2811 (control port), we 
+        
+        flow_key = conn_obj['orig_h'] + '-' + conn_obj['resp_h']
+        if FLOWS_CACHE.has_key(flow_key):
+            #We update the flow entry port range if necessary
+            
+            logging.debug('We have a flow going this direction!!')
+        else:
+            #We create a new flow entry into the cache. Port entries are first assumed to be the minimum value
+            
+            FLOWS_CACHE[ flow_key ] = {
+                                       'orig_p_range': [conn_obj['orig_p'], None],
+                                       'resp_p_range': [conn_obj['resp_p'], None],
+                                       'time': time.time()
+                                       }
+            logging.debug(FLOWS_CACHE)
         
     
     def finish(self):
