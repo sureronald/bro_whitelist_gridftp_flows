9c790125a91dd68c3a47ed879a02b6279d581a64
Ronald Osure <sureronald@gmail.com>
2013-06-27 21:37:00 +0300

    iU-2013.27.06-R-0 Commence writing of unit tests

---
+++ tests.py
@@ -0,0 +1,28 @@
+#Unit Tests
+
+import os
+import sys
+import unittest
+
+__author__ = 'Ronald Osure (rosure@indiana.edu)'
+__copyright__ = 'Copyright 2012, InCNTRE. This file is licensed under Apache 2.0'
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+
+if PATH not in sys.path:
+    sys.path.append(PATH)
+
+import flow_handler
+
+class TestFlowHandler(unittest.TestCase):
+    
+    def test_host_port(self):
+        self.assertEqual(('localhost', 1999,), flow_handler.HOST_PORT)
+    
+    def test_log_path(self):
+        self.assertGreater(flow_handler.LOGGING_PATH, '', 'No log path given!')
+
+if __name__=='__main__':
+    unittest.main()
+
+            
